faa5c1a03fe909b649357504832ea7368c1de746
ManuOmulo <omuloemmanuel@gmail.com>
2023-12-06 13:01:05 +0300

    Classes Challenge

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,3 +1,80 @@
 public class BankAccount {
 
+  private int accountNumber;
+  private double accountBalance;
+  private String customerName;
+  private String email;
+  private String phoneNumber;
+
+
+// --------------- Getters ----------------
+  public int getAccountNumber() {
+    return accountNumber;
+  }
+
+  public double getAccountBalance() {
+    return accountBalance;
+  }
+
+  public String getCustomerName() {
+    return customerName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+
+// --------------- Setters -----------------
+  public void setAccountNumber(int accountNumber) {
+    this.accountNumber = accountNumber;
+  }
+
+  public void setAccountBalance(double accountBalance) {
+    this.accountBalance = accountBalance;
+  }
+
+  public void setCustomerName(String customerName) {
+    this.customerName = customerName;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+
+  public void getAccountDetails() {
+    System.out.println(
+        "Acc. Number: " + this.accountNumber + "\n" +
+        "Customer Name: " + this.customerName + "\n" +
+        "Acc. Balance: " + this.accountBalance + "\n" +
+        "Phone No.: " + this.phoneNumber + "\n" +
+        "Email: " + this.email
+    );
+  }
+
+  public void deposit(double amount) {
+    double newBalance = this.accountBalance + amount;
+    this.accountBalance = newBalance;
+    System.out.println("Deposited Ksh." + amount + ". New Account Balance is Ksh." + newBalance);
+  };
+
+  public void withdraw(double amount) {
+    double newBalance = this.accountBalance - amount;
+
+    if (newBalance < 0) {
+      System.out.println("Cannot withdraw Ksh." + amount + ". Current balance of " + this.accountBalance + " is insufficient.");
+    } else if (newBalance >= 0) {
+      this.accountBalance = newBalance;
+      System.out.println("Ksh." + amount + " has been withdrawn. Current balance is Ksh." + newBalance);
+    }
+  };
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
   public static  void main(String[] args) {
+    BankAccount acc1 = new BankAccount();
 
+    acc1.setAccountNumber(12220229);
+    acc1.setAccountBalance(200_000);
+    acc1.setCustomerName("Emmanuel Omulo");
+    acc1.setPhoneNumber("+254 705 994281");
+    acc1.setEmail("eomulo@protonmail.com");
+
+    acc1.getAccountDetails();
+
+    acc1.deposit(50_000);
+    acc1.withdraw(22_000);
+    acc1.withdraw(30_000.57);
+    acc1.withdraw(197999.43);
+
+    acc1.getAccountDetails();
   }
 }
